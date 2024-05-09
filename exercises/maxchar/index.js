12b554b35b2ddc1d0191b1aca61ee070877355d1
Sofía González <sofia_1.6@hotmail.com>
2024-05-09 16:39:40 -0300

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let maxKey = null;
+  let maxValue = 0;
+
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+
+  for (let key in chars) {
+    if (chars[key] > maxValue) {
+      maxValue = chars[key];
+      maxKey = key;
+    }
+  }
+  return maxKey;
+}
 
 module.exports = maxChar;
