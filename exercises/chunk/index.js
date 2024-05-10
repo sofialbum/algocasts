f13932215bc07f9afc2baff453e672a1aadd6501
Sofía González <sofia_1.6@hotmail.com>
2024-05-10 19:11:02 -0300

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+
+function chunk(arr, size) {
+  const chunks = [];
+  
+  for (let i = 0; i < arr.length; i += size) {
+    let chunky = arr.slice(i, i + size);
+    chunks.push(chunky);
+  }
+
+  console.log("este es", chunks);
+  return chunks;
+}
 
 module.exports = chunk;
