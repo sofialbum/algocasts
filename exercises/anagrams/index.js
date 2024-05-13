d2d3b08ceb134577967ab874da349903dba91f03
Sofía González <sofia_1.6@hotmail.com>
2024-05-13 16:29:59 -0300

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const firstStr = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const secondStr = stringB.replace(/[^\w]/g, "").toLowerCase();
+  const firstChars = {};
+  const secondChars = {};
+
+  console.log("firstStr length", firstStr.length, "secondStr length", secondStr.length);
+
+  if(firstStr.length === secondStr.length) {
+    for (let char of firstStr) {
+      if (!firstChars[char]) {
+        firstChars[char] = 1;
+      } else {
+        firstChars[char]++;
+      }
+    }
+  
+    for (let char of secondStr) {
+      if (!secondChars[char]) {
+        secondChars[char] = 1;
+      } else {
+        secondChars[char]++;
+      }
+    }
+
+    function sortedObject(unordered) {
+      return Object.keys(unordered).sort().reduce(
+        (obj, key) => {
+          obj[key] = unordered[key];
+          return obj;
+        }, {});
+    }
+
+  return JSON.stringify(sortedObject(firstChars)) === JSON.stringify(sortedObject(secondChars))
+
+  } else {
+    return false;
+  }
+}
 
 module.exports = anagrams;
+ 
