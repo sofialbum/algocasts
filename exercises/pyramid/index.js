c053fe06351dacc2c8fe109b53ee3383351a2ef8
Sofía González <sofia_1.6@hotmail.com>
2024-05-15 22:06:41 -0300

    pyramidrecursive

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,27 +14,50 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  for (let i = 0; i < n; i++){
-    let level = '';
+// function pyramid(n) {
+//   for (let i = 0; i < n; i++){
+//     let level = '';
 
-    const totalLength = (n - 1) + n;
-    const middle = Math.floor(totalLength / 2)
+//     const totalLength = (n - 1) + n;
+//     const middle = Math.floor(totalLength / 2)
 
-    const inicio = middle - i;
-    const final = middle + i;
+//     const inicio = middle - i;
+//     const final = middle + i;
 
 
-    for (let j = 0; j < totalLength; j++){
-      if (j >= inicio && j <= final) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+//     for (let j = 0; j < totalLength; j++){
+//       if (j >= inicio && j <= final) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level)
+//   }
+// }
 
-    console.log(level)
+function pyramid(n, row = 0, level = '', column = 0) {
+  const totalLength = 2 * n - 1;
+  const middle = Math.floor(totalLength / 2);
+  const inicio = middle - row;
+  const final = middle + row;
+
+  if (n === row && column === totalLength) {
+    return;
   }
+
+  if (level.length === totalLength) {
+    console.log(level);
+    return pyramid(n, row + 1, '', 0);
+  }
+
+  if (column >= inicio && column <= final) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+  pyramid(n, row, level, column + 1)
+
 }
 
 module.exports = pyramid;
