8eed8412d55f0380d9e99dd424eb17560babac3d
Sofía González <sofia_1.6@hotmail.com>
2024-05-21 16:14:55 -0300

    memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,17 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  let fibonacci = [0, 1];
-  
-  while(fibonacci.length <= n) {
-    let sum = fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]
-    fibonacci.push(sum);
-    
-    
-  }
-  console.log(fibonacci[fibonacci.length -1])
-  return fibonacci[fibonacci.length -1];
+// RECURSIVE SOLUTION AND MEMOIZATION
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
 }
 
+
+function fib(n) {
+  if (n < 2)  {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
+
 module.exports = fib;
+
+// function fib(n) {
+//   let fibonacci = [0, 1];
+  
+//   while(fibonacci.length <= n) {
+//     let sum = fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]
+//     fibonacci.push(sum);
+//   }
+//   return fibonacci[n];
+// }
+
+// module.exports = fib;
