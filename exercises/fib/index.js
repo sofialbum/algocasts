48cf4fd42a332e403db537a68f76e8d2fd0a9a26
Sofía González <sofia_1.6@hotmail.com>
2024-05-20 22:07:05 -0300

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let fibonacci = [0, 1];
+  
+  while(fibonacci.length <= n) {
+    let sum = fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]
+    fibonacci.push(sum);
+    
+    
+  }
+  console.log(fibonacci[fibonacci.length -1])
+  return fibonacci[fibonacci.length -1];
+}
 
 module.exports = fib;
