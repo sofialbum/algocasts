e6724f073b3ec484f3d9a2b2273e6ac6ccb9c0b9
Sofía González <sofia_1.6@hotmail.com>
2024-05-22 17:56:33 -0300

    qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,44 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(record) {
+    this.stack1.push(record)
+  }
+
+  remove() {
+    while(this.stack1.peek()) {
+      const popped = this.stack1.pop()
+      this.stack2.push(popped)
+    }
+    const firstPop = this.stack2.pop()
+
+    while(this.stack2.peek()){
+      const poppeddd = this.stack2.pop()
+      this.stack1.push(poppeddd)
+    }
+    return firstPop;
+  }
+
+  peek(){
+    while(this.stack1.peek()) {
+      const popped = this.stack1.pop()
+      this.stack2.push(popped)
+    }
+
+    const firstPeek = this.stack2.peek()
+    
+    while(this.stack2.peek()){
+      const poppeddd = this.stack2.pop()
+      this.stack1.push(poppeddd)
+    }
+    return firstPeek;
+  }
+}
 
 module.exports = Queue;
