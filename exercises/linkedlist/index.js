3b833cafab5293566ed8ab2032a09cb23b9dec88
Sofía González <sofia_1.6@hotmail.com>
2024-05-23 12:12:14 -0300

    linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,31 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+}
 
 module.exports = { Node, LinkedList };
