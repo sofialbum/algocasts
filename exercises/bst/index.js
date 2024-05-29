9256b8527c47054745d449d7ba75e0f19041d365
Sofía González <sofia_1.6@hotmail.com>
2024-05-29 18:28:13 -0300

    moreonbsr\t

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -29,5 +29,19 @@ class Node {
       this.right = new Node(data);
     }
   }
+
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data)
+    } else if (this.data > data && this.left) {
+    return this.left.contains(data)
+    }
+
+    return null;
+  }
 }
 module.exports = Node;
