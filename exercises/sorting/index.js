b2b9d8fdf88d74988c9adf6038ed557949417f7b
Sofía González <sofia_1.6@hotmail.com>
2024-05-30 18:12:27 -0300

    bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,21 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  // arr.sort(function(a, b){ 
+  //   return a - b
+  // })
 
+  // return arr;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = lesser
+      }
+    }
+  }
+  return arr
 }
 
 function selectionSort(arr) {
