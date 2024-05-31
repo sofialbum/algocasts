15337364426f48d121fbfae4beb48977b8b65a0f
Sofía González <sofia_1.6@hotmail.com>
2024-05-31 17:00:19 -0300

    merge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -42,6 +42,18 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+}
+
+
+return [...results, ...left, ...right];
 
 }
 
